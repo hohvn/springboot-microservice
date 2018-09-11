6975f54dbaa2aa3e2737389bf861c7fd53787a42
tran.van.thai <tran.van.thai@linecorp.com>
2018-09-11 16:04:02 +0700

    add monitoring server

---
+++ monitoring/src/main/java/com/thaitran/microservices/monitoring/MonitoringApplication.java
@@ -0,0 +1,14 @@
+package com.thaitran.microservices.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class MonitoringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringApplication.class, args);
+    }
+}
